2751d86633929a9c2087c56af3f3aa279e8af32e
Ashish Juyal <ashish@thecodecamp.in>
2023-08-09 17:53:58 +0530

    ISP example

--- src/main/java/isp/Reportable.java
+++
@@ -1,9 +0,0 @@
-package isp;
-
-import java.io.BufferedReader;
-
-public interface Reportable {
-  public byte[] generateExcel(BufferedReader reader);
-
-  public byte[] generatePdf(BufferedReader reader);
-}
